829205b7511941f13a5cae2b2de6259b1c14f985
Szkrishna <krisnas9792@gmail.com>
2025-04-02 00:04:10 +0530

    hero section

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Hero() {
+  return (
+    <>
+        <div className="hero" style={{ minHeight: "58vh" }}>
+          <h5 className='mt-3'>Hero Section</h5>
+        </div>
+    </>
+  )
+}
